b7034f83c85893bececa8c211e548df8493c7a13
Francesca Bueti <fbueti@gmail.com>
2017-11-24 18:14:44 -0500

    made playbook-nav change colors

--- dist/js/playbook-nav.js
+++ dist/js/playbook-nav.js
@@ -12,27 +12,23 @@ function touching($div1, $div2) {
 }
 
 $(window).scroll(function() {
-    var nav = $("#playbook-nav");
-    var blue = $(".blue");
-    var white = $(".white");    
-    var nav_top = nav.offset().top;
-    var blue_top = blue.offset().top;
-    var white_top = white.offset().top;
-    var nav_bottom = nav_top + nav.height();
-    var blue_bottom = blue_top + blue.height();
-    var white_bottom = white_top + white.height();
-
-    if (touching($("#intro"), $("#playbook-nav"))){
-      console.log("hello");
-    }
-    if (nav_bottom >= blue_top && nav_top < blue_bottom) {
-      $( "#playbook-nav > li > a > div" ).addClass( "light-circle" );
-      $( "#playbook-nav > li > a > div" ).removeClass( "dark-circle" );
-      $( "#playbook-nav > li" ).css({'color': 'white'});
-    }
-    if (nav_bottom >= white_top && nav_top < white_bottom) {
+    if (touching($("#intro"), $("#playbook-nav")) ||
+        touching($("#02"), $("#playbook-nav")) ||
+        touching($("#04"), $("#playbook-nav")) ||
+        touching($("#06"), $("#playbook-nav"))
+      ){
       $( "#playbook-nav > li > a > div" ).addClass( "dark-circle" );
       $( "#playbook-nav > li > a > div" ).removeClass( "light-circle" );
       $( "#playbook-nav > li" ).css({'color': 'black'});
+    }
+
+    if (touching($("#01"), $("#playbook-nav")) ||
+        touching($("#03"), $("#playbook-nav")) ||
+        touching($("#05"), $("#playbook-nav")) ||
+        touching($("#07"), $("#playbook-nav"))
+      ){
+      $( "#playbook-nav > li > a > div" ).addClass( "light-circle" );
+      $( "#playbook-nav > li > a > div" ).removeClass( "dark-circle" );
+      $( "#playbook-nav > li" ).css({'color': 'white'});
     }
 });
